bb7e2ea3dc770221e72f8951e4eb28ab758fa7f9
Roy Mosby <ego.madking@gmail.com>
2021-08-13 09:51:20 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import Buttons from './components/Buttons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="section">
+      <div className="container">
+        <h1 className="title has-text-centered">Bulma</h1>
+        <p className="subtitle has-text-centered">
+          Modern CSS framework based on{' '}
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
+            Flexbox
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="Input" />
+          </div>
+        </div>
+
+        <div className="field">
+          <p className="control">
+            <span className="select">
+              <select>
+                <option>Select dropdown</option>
+              </select>
+            </span>
+          </p>
+        </div>
+        <Buttons/>
+        
+      </div>
     </div>
   );
 }
