6d0d3513a3497c42a5ffd3cc0b2fd17167424d28
Roy Mosby <ego.madking@gmail.com>
2021-08-16 17:27:01 -0400

    adds Router functionality

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 import './App.scss';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
-import Buttons from './components/Buttons';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Info from './components/Info';
+import Projects from './components/Projects'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="section">
-      <div className="container">
-        <h1 className="title has-text-centered">Bulma</h1>
-        <p className="subtitle has-text-centered">
-          Modern CSS framework based on{' '}
-          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
-            Flexbox
-          </a>
-        </p>
+    <Router>
 
-        <div className="field">
-          <div className="control">
-            <input className="input" type="text" placeholder="Input" />
-          </div>
+      <div className="section">
+        <div className="container">
+          <Header/>
+          <Hero/>
+          <Info/>
+          <Projects/>
+          <Footer/>
         </div>
-
-        <div className="field">
-          <p className="control">
-            <span className="select">
-              <select>
-                <option>Select dropdown</option>
-              </select>
-            </span>
-          </p>
-        </div>
-        <Buttons/>
-        
       </div>
-    </div>
+      
+    </Router>
   );
 }
 
