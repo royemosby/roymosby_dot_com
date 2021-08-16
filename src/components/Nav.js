f7b1452c6bf115cf6897960fc6923d9317c3408e
Roy Mosby <ego.madking@gmail.com>
2021-08-16 17:28:13 -0400

    creates initial nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Nav(){
+  return(
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <h1 className="title">It's'a me! Roy-e-o!!</h1>
+      </div>
+    </nav>
+  )
+}
