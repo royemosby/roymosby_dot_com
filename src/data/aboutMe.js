95c71a04e991b98739158c52d1783ff4e22d9a5c
Roy Mosby <ego.madking@gmail.com>
2021-08-16 17:28:45 -0400

    adds bio info

---
+++ src/data/aboutMe.js
@@ -0,0 +1,19 @@
+const shortBioArray = ["I am a self-identified elder millennial. Married. 1 kid. Cats. Military retiree. I'm a fan of flat caps, cast iron cooking, and web technologies. Oh, and my sourdough starter's name is \"Specimen Gamma\".", "I specialize in Ruby, Ruby on Rails, JavaScript, React, and all the fun tie-ins in between. I am open to career opportunities."]
+
+const longBioArray = [
+"Hi! My name is Roy and I am a recent Flatiron graduate. I took the software engineer bootcamp focused on Ruby, Ruby on Rails, JavaScript, React and everything between. I also have 20 years military communications experience so bring a great deal of expertise to the table.",
+
+"I my interest in development came from the last few positions that I held. One of my responsibilities was I was making dashboards and other presentational items out of HTML, CSS, and JavaScript. I found that to be the most challenging and rewarding part of my job and decided that I wanted to dive deeper into that.",
+
+"I'm looking to make a positive impact at an organization that sees inclusivity as a strength rather than an obligation."]
+
+
+const contactInfo = {
+  twitter: "https://twitter.com/royemosby",
+  linkedIn: "https://www.linkedin.com/in/roy-mosby/",
+  gitHub: "https://github.com/royemosby",
+  codePen: "https://codepen.io/royemosby",
+  blog: "https://www.roymosby.me/"
+}
+
+export {shortBioArray, longBioArray, contactInfo}
