8a91b5744f14fa953a27aa0fef59ebfaf895228f
Roy Mosby <ego.madking@gmail.com>
2021-08-17 16:51:45 -0400

    adds project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,19 @@
+import {projects} from "../data/projects"
+import ProjectCard from "./ProjectCard/ProjectCard"
+
 export default function Projects(){
+
+  function addProjectCards(){
+    return(
+      projects.map((proj, key) => <ProjectCard project={proj} key={key}/>)
+    )
+  }
   return(
     <div className="box">
-      <h2 className="is-size-4">Projectseses</h2>
+      <h2 className="is-size-2 has-text-centered">Projects</h2>
+        <div className="tile is-ancestor is-flex-wrap-wrap is-align-content-space-between">
+      {addProjectCards()}
+      </div>
     </div>
   )
 }
