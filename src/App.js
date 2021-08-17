c38bc96399eb9b8aba2f5e3510316b1769c5e759
Roy Mosby <ego.madking@gmail.com>
2021-08-17 16:49:19 -0400

    adds contact component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Info from './components/Info';
+import Contact from './components/Contact';
 import Projects from './components/Projects'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
@@ -16,6 +17,7 @@ function App() {
           <Hero/>
           <Info/>
           <Projects/>
+          <Contact/>
           <Footer/>
         </div>
       </div>
