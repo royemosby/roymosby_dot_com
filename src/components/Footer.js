1c1fa7eaec561818a50a62c5d860b626f761d621
Roy Mosby <ego.madking@gmail.com>
2021-08-17 16:51:07 -0400

    resizes and centers component heading

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer(){
   return(
     <div className="box">
-      <h2 className="is-size-4">Footsie!!</h2>
+      <h2 className="is-size-2 has-text-centered">Footsie!!</h2>
     </div>
   )
 }
