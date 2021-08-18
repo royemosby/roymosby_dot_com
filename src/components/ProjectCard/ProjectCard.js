c982cf05c23f9517121528df97416a8c725dd0be
Roy Mosby <ego.madking@gmail.com>
2021-08-18 15:27:28 -0400

    fleshes out project card

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,36 +1,45 @@
-import { projects } from "../../data/projects";
-import coinStacks from "../../assets/coin-stacks.jpg"
 
-function mapTags(technologies){
-  return technologies.map((tech, key)=>{
-    return(
-        <span className="tag is-info" key={key}>{tech}</span>
-    ) 
-  })
-}
+
+import Tags from './Tags'
+import Challenge from './Challenge'
+import styles from './ProjectCard.module.scss'
+
 
 export default function ProjectCard({project}){
   return(
-    <div className="tile is-6 p-4">
-
-      <div className="card">
-        <header className="card-header p-4">
-          <p className="card-title is-size-3">
-            {project.name}
-          </p>
-        </header>
+    <div className={["tile is-6 p-4", styles.card].join(" ")}>
+      <div className={["card", styles.overflowScroll].join(" ")}>
+        <div className={styles.front}>
+          <header className="card-header p-4">
+            <h3 className="card-title is-size-3">
+              {project.name}
+            </h3>
+          </header>
+          <div className="card-image p-2">
+            <figure>
+              <img src={project.preview_image} alt={`${project.name} preview`}/>
+              <figcaption>
+                <div className="tags">
+                  <Tags tech={project.technologies}/>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
           <div className="card-content">
             {project.description}
           </div>
-        <div className="card-image p-2">
-          <figure>
-            <img src={project.preview_image} alt={`${project.name} preview`}/>
-            <figcaption>
-              <div className="tags">
-                {mapTags(project.technologies)}
-              </div>
-            </figcaption>
-          </figure>
+        </div>
+        <div className={[styles.back].join(" ")}>
+          <header className="card-header p-4">
+            <h3 className="card-title is-size-3">
+              {project.name} details
+            </h3>
+          </header>
+          <div className="card-content">
+            <div className="content">
+              <Challenge challenge={project.challenge}/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
