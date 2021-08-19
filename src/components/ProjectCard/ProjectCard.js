b52c9eca6691ebcee908e1500bca84128ea82e63
Roy Mosby <ego.madking@gmail.com>
2021-08-19 07:03:47 -0400

    adds Links component, adds .title to headings

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,7 +1,7 @@
 import Tags from './Tags'
 import Challenge from './Challenge'
 import styles from './ProjectCard.module.scss'
-
+import Links from './Links'
 
 export default function ProjectCard({project}){
   return(
@@ -9,7 +9,7 @@ export default function ProjectCard({project}){
       <div className={["card", styles.overflowScroll].join(" ")}>
         <div className={styles.front}>
           <header className="card-header p-4">
-            <h3 className="card-title is-size-3">
+            <h3 className=" title card-title is-size-3">
               {project.name}
             </h3>
           </header>
@@ -29,13 +29,15 @@ export default function ProjectCard({project}){
         </div>
         <div className={[styles.back].join(" ")}>
           <header className="card-header p-4">
-            <h3 className="card-title is-size-3">
+            <h3 className="card-title is-size-3 title">
               {project.name} details
             </h3>
           </header>
           <div className="card-content">
+            <h4 className="card-title is size-5 subtitle">Challenge:</h4>
             <div className="content">
               <Challenge challenge={project.challenge}/>
+              <Links name = {project.name} links={project.links}/>
             </div>
           </div>
         </div>
