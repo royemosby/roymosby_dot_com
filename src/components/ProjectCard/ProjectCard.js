7a67d58e430dd51672693ee36fa01377500329ce
Roy Mosby <ego.madking@gmail.com>
2021-09-15 22:10:44 -0400

    adds css flip classes

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,13 +1,29 @@
+import { useState } from 'react'
 import Tags from './Tags'
 import Challenge from './Challenge'
 import styles from './ProjectCard.module.scss'
 import Links from './Links'
+import Button from './Button'
+
+
 
 export default function ProjectCard({project}){
+  const unflippedStyles = ["card", styles.overflowScroll]
+
+  const [flippedClasses, setFlippedClasses] = useState(unflippedStyles.join(" "))
+
+  const flipCard = () => {
+    if (flippedClasses === unflippedStyles.join(" ")){
+      setFlippedClasses([...unflippedStyles, styles.isFlippedY].join(" "))
+    } else {
+      setFlippedClasses(unflippedStyles.join(" "))
+    }
+  }
+
   return(
     <div className={["tile is-6 p-4", styles.card].join(" ")}>
-      <div className={["card", styles.overflowScroll].join(" ")}>
-        <div className={styles.front}>
+      <div className={flippedClasses}>
+        <div className={[styles.front, "pb-4"].join(" ")}>
           <header className="card-header p-4">
             <h3 className=" title card-title is-size-3">
               {project.name}
@@ -27,7 +43,7 @@ export default function ProjectCard({project}){
             {project.description}
           </div>
         </div>
-        <div className={[styles.back].join(" ")}>
+        <div className={[styles.back, "pb-4"].join(" ")}>
           <header className="card-header p-4">
             <h3 className="card-title is-size-3 title">
               {project.name} details
@@ -41,7 +57,9 @@ export default function ProjectCard({project}){
             </div>
           </div>
         </div>
+
       </div>
+        <Button flipCard={flipCard}/>
     </div>
   )
 }
